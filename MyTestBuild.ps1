728a1d642c2ec96174a1f1697f1fe405ff77f0ca
passengerJIA <johnzhong@hotmail.com>
2020-01-15 15:44:08 +0800

    Cheers

--- MyTestBuild.ps1
+++ MyTestBuild.ps1
@@ -1,5 +1,6 @@
 function Pack(){
-    nuget restore .\SelfTest.sln
-    nuget pack "SelfTestWebMVCKendo\SelfTestWebMVCKendo.csproj" -Build -Symbols -Properties Configuration=Release
+    $msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\bin\MSBuild.exe"
+    $colletionOfArgs = @(".\SelfTestWebMVCKendo\SelfTestWebMVCKendo.csproj","-p:configuration=release")
+    & $msbuild $colletionOfArgs
 }
 Pack
